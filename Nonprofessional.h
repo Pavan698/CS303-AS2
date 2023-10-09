e53a469dacd6309e85e430a947739da62a3ac419
Pavan Krishna Karri <pkcgb@umsystem.edu>
2023-10-08 21:38:12 -0500

    Update Nonprofessional.h

--- Nonprofessional.h
+++ Nonprofessional.h
@@ -1,1 +1,22 @@
+#ifndef NONPROFESSIONAL_H
+#define NONPROFESSIONAL_H
+
+#include "Employee.h"
+
+class Nonprofessional : public Employee {
+public:
+    // Constructor: Initialize a Nonprofessional object with a name, employee ID, hourly rate, and worked hours.
+    Nonprofessional(const std::string& name, int employeeID, double hourlyRate, int workedHours);
+
+    // Override functions inherited from the base class.
+    double calculateWeeklySalary() const override;
+    double calculateHealthCareContributions() const override;
+    int calculateVacationDaysEarned() const override;
+
+private:
+    double hourlyRate;
+    int workedHours;
+};
+
+#endif 
 
